fed29ac572c259b795a869d1a43e151cd420ed34
김하챤 <dbtldk12@naver.com>
2025-03-01 17:52:45 +0900

    Fix : ExitBtn
    진짜종료할건지물어봄

--- Assets/Test_Sia/PanelOpen.cs
+++ Assets/Test_Sia/PanelOpen.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class PanelOpen : MonoBehaviour
 {
     public GameObject optionPanel; // 옵션 패널 연결할 변수
-    public GameObject savePanel; // 옵션 패널 연결할 변수
+    public GameObject savePanel; // 이어하기 패널 연결할 변수
+    public GameObject exitPanel; // 이어하기 패널 연결할 변수
     public GameObject blockerPanel;   // 클릭 차단용 패널
 
     public void ToggleOptionMenu()
@@ -13,6 +14,7 @@ public class PanelOpen : MonoBehaviour
         blockerPanel.SetActive(isActive); // 패널이 열릴 때만 클릭 차단 활성화
         optionPanel.SetActive(isActive);
         savePanel.SetActive(false); // 이어하기 창은 닫힘
+        exitPanel.SetActive(false); // 종료 창은 닫힘
     }
     public void ToggleSaveMenu()
     {
@@ -20,6 +22,16 @@ public class PanelOpen : MonoBehaviour
         // 옵션 패널의 활성/비활성 상태를 전환
         blockerPanel.SetActive(isActive); // 패널이 열릴 때만 클릭 차단 활성화
         savePanel.SetActive(isActive);
+        optionPanel.SetActive(false); // 옵션 창은 닫힘
+        exitPanel.SetActive(false); // 종료 창은 닫힘
+    }
+    public void ToggleExitMenu()
+    {
+        bool isActive = !exitPanel.activeSelf;
+        // 옵션 패널의 활성/비활성 상태를 전환
+        blockerPanel.SetActive(isActive); // 패널이 열릴 때만 클릭 차단 활성화
+        exitPanel.SetActive(isActive);
+        savePanel.SetActive(false); // 이어하기 창은 닫힘
         optionPanel.SetActive(false); // 옵션 창은 닫힘
     }
     // 옵션 창 닫기
@@ -33,6 +45,12 @@ public class PanelOpen : MonoBehaviour
     public void CloseSaveMenu()
     {
         savePanel.SetActive(false);
+        blockerPanel.SetActive(false); // 닫힐 때 차단 해제
+    }
+    // 종료 창 닫기
+    public void CloseExitMenu()
+    {
+        exitPanel.SetActive(false);
         blockerPanel.SetActive(false); // 닫힐 때 차단 해제
     }
 }
